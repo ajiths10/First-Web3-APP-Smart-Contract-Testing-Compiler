b704bb10e0a8f54cc6f3938b05a7036b4c05e856
Ajith S <ajithsnair10@gmail.com>
2024-06-06 12:43:53 +0530

    ganache testing learning part

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,31 @@
-const ganache = require('ganache');
-const { Web3 } = require('web3');
+const assert = require("assert");
+const ganache = require("ganache");
+const { Web3 } = require("web3");
 
-// updated ganache and web3 imports added for convenience
+const web3 = new Web3(ganache.provider());
 
-// contract test code will go here
+// ganache sample
+// For ref
+class Car {
+  park() {
+    return "stopped";
+  }
+
+  drive() {
+    return "vromm";
+  }
+}
+let car;
+beforeEach(() => {
+  console.log("beforeEach here");
+  car = new Car();
+});
+describe("Car", () => {
+  it("can park", () => {
+    assert.equal(car.park(), "stopped");
+  });
+
+  it("can drive", () => {
+    assert.equal(car.drive(), "vromm");
+  });
+});
