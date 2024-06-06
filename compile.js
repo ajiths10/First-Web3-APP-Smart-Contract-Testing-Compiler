13bb1dac92ffd4423406a9b8792ff3c76d3f0e1e
Ajith S <ajithsnair10@gmail.com>
2024-06-06 15:34:06 +0530

    deploment with web3

--- compile.js
+++ compile.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
-const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sool");
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 const source = fs.readFileSync(inboxPath, "utf-8");
 
 //Export
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,31 +1,54 @@
 const assert = require("assert");
 const ganache = require("ganache");
 const { Web3 } = require("web3");
+const { interface, bytecode } = require("../compile");
 
 const web3 = new Web3(ganache.provider());
 
-// ganache sample
-// For ref
-class Car {
-  park() {
-    return "stopped";
-  }
+let accounts;
+let inbox;
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
 
-  drive() {
-    return "vromm";
-  }
-}
-let car;
-beforeEach(() => {
-  console.log("beforeEach here");
-  car = new Car();
+  // Use one of those accounts to deploy
+  // the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
 });
-describe("Car", () => {
-  it("can park", () => {
-    assert.equal(car.park(), "stopped");
-  });
 
-  it("can drive", () => {
-    assert.equal(car.drive(), "vromm");
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(inbox);
   });
 });
+
+// // ganache sample
+// // For ref
+// class Car {
+//   park() {
+//     return "stopped";
+//   }
+
+//   drive() {
+//     return "vromm";
+//   }
+// }
+// let car;
+// beforeEach(() => {
+//   console.log("beforeEach here");
+//   car = new Car();
+// });
+// describe("Car", () => {
+//   it("can park", () => {
+//     assert.equal(car.park(), "stopped");
+//   });
+
+//   it("can drive", () => {
+//     assert.equal(car.drive(), "vromm");
+//   });
+// });
